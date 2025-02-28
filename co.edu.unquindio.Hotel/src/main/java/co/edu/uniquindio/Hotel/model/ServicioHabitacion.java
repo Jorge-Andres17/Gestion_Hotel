00101810d209b9916b44a6abc2fcc814dd88ca9c
Jorge-Andres17 <jorgea.ibarrag@uqvirtual.edu.co>
2025-02-27 22:02:34 -0500

    Actualizacion del codigo base
    se agregan relacciones entre clases, crud de las listas, enums necesarios.

--- co.edu.unquindio.Hotel/src/main/java/co/edu/uniquindio/Hotel/model/ServicioHabitacion.java
+++ co.edu.unquindio.Hotel/src/main/java/co/edu/uniquindio/Hotel/model/ServicioHabitacion.java
@@ -1,4 +1,14 @@
 package co.edu.uniquindio.Hotel.model;
 
-public class ServicioHabitacion {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServicioHabitacion extends Servicio{
+    private List<Habitacion> listaHabitaciones = new ArrayList();
+
+    public ServicioHabitacion(){}
+
+    public ServicioHabitacion(int idServicio, String nombreServicio, double precioServicio) {
+        super(idServicio, nombreServicio, precioServicio);
+    }
 }
